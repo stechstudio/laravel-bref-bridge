dcf70e3debdda35a56a961e8e874dcb5c4169198
Bubba <rob@stechstudio.com>
2019-02-22 17:57:48 -0500

    Artisan Configure SAM command

---
+++ src/Console/ConfigSam.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace STS\Bref\Bridge\Console;
+
+use Illuminate\Console\Command;
+use STS\Bref\Bridge\Events\SamConfigurationRequested;
+
+class ConfigSam extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'bref:config-sam';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Configure the SAM Template.';
+
+    public function handle(): int
+    {
+        event(new SamConfigurationRequested);
+        return 0;
+    }
+}
