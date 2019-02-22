a388a8f8d831767b658e7679545c466ea99b4283
Bubba <rob@stechstudio.com>
2019-02-22 18:01:49 -0500

    Update Function Service

---
+++ src/Services/UpdateFunction.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace STS\Bref\Bridge\Services;
+
+use STS\Bref\Bridge\Events\UpdateRequested;
+use Symfony\Component\Process\Process;
+
+class UpdateFunction
+{
+    public function handle(UpdateRequested $event): void
+    {
+        $this->runUpdate(config('bref.website_name'));
+        $this->runUpdate(config('bref.artisan_name'));
+    }
+
+    protected function runUpdate(string $functionName): void
+    {
+        $process = new Process([
+            'aws',
+            'lambda update-function-code',
+            '--function-name',
+            $functionName,
+            '--zip-file',
+            'fileb://storage/latest.zip',
+        ]);
+        $process->setWorkingDirectory(base_path());
+        $process->setTimeout(600);
+        $process->start();
+
+        foreach ($process as $type => $data) {
+            echo $data;
+        }
+    }
+}
