40459993be58120b956b9d08692c5c0244146303
Bubba <rob@stechstudio.com>
2019-02-22 17:58:31 -0500

    Artisan Start API command

---
+++ src/Console/StartApi.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+
+/**
+ * User: bubba
+ * Date: 2019-02-01
+ * Time: 14:17
+ */
+
+namespace STS\Bref\Bridge\Console;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
+
+class StartApi extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'bref:start-api';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Starts up the SAM Local API for testing.';
+
+    public function handle(): int
+    {
+        $process = new Process(['sam', 'local', 'start-api']);
+        $process->setWorkingDirectory(base_path());
+        $process->start();
+
+        foreach ($process as $type => $data) {
+            echo $data;
+        }
+        return 0;
+    }
+}
