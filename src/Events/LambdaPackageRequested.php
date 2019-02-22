53795566b9978ae85255ed780ef600c1ef522d10
Bubba <rob@stechstudio.com>
2019-02-22 17:59:12 -0500

    Lambda Package Requested Event

---
+++ src/Events/LambdaPackageRequested.php
@@ -0,0 +1,60 @@
+<?php declare(strict_types=1);
+
+namespace STS\Bref\Bridge\Events;
+
+use STS\Bref\Bridge\Console\Package;
+
+class LambdaPackageRequested
+{
+    /**
+     * Holds a reference to the console command for I/O
+     *
+     * @var Package
+     */
+    protected $console;
+
+    /**
+     * Console Command Getter
+     */
+    public function getConsole(): Package
+    {
+        return $this->console;
+    }
+
+    /**
+     * Console command setter
+     */
+    public function setConsole(Package $console): LambdaPackageRequested
+    {
+        $this->console = $console;
+        return $this;
+    }
+
+    /**
+     * Console Info Logging
+     */
+    public function info(string $message): void
+    {
+        if ($this->isFromConsole()) {
+            $this->console->info($message);
+        }
+    }
+
+    /**
+     * Determine if the event is from a console.
+     */
+    public function isFromConsole(): bool
+    {
+        return isset($this->console);
+    }
+
+    /**
+     * Console error logging
+     */
+    public function error(string $message): void
+    {
+        if ($this->isFromConsole()) {
+            $this->console->error($message);
+        }
+    }
+}
