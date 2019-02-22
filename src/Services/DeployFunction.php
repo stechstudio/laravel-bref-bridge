604bd75128d0bc549e7c69cbd5df8298bba892af
Bubba <rob@stechstudio.com>
2019-02-22 18:00:58 -0500

    Deploy Function Service

---
+++ src/Services/DeployFunction.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace STS\Bref\Bridge\Services;
+
+use STS\Bref\Bridge\Events\DeploymentRequested;
+use Symfony\Component\Process\Process;
+
+class DeployFunction
+{
+    public function handle(DeploymentRequested $event): void
+    {
+        $process = new Process([
+            'sam',
+            'deploy',
+            '--template-file',
+            '.stack.yaml',
+            '--capabilities',
+            'CAPABILITY_IAM',
+            '--stack-name',
+            config('bref.name'),
+        ]);
+        $process->setWorkingDirectory(base_path());
+        $process->setTimeout(600);
+        $process->start();
+
+        foreach ($process as $type => $data) {
+            echo $data;
+        }
+    }
+}
