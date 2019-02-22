a04db5daa53e6d2f0f5e038177aa5484000f5153
Bubba <rob@stechstudio.com>
2019-02-22 18:00:00 -0500

    Package Exception

---
+++ src/Exceptions/Package.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+/**
+ * User: bubba
+ * Date: 2019-01-31
+ * Time: 16:34
+ */
+
+namespace STS\Bref\Bridge\Exceptions;
+
+class Package extends \RuntimeException
+{
+}
