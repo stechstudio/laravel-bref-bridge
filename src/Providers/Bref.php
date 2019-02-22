0cd3f50df96ad37813fac15e95c90d05f34ceff7
Bubba <rob@stechstudio.com>
2019-02-22 17:27:12 -0500

    Stub service provider

---
+++ src/Providers/Bref.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace STS\Bref\Bridge\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class Bref extends ServiceProvider
+{
+    public function register(): void
+    {
+    }
+
+    public function boot(): void
+    {
+
+    }
+}
