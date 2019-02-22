4fc5ed0d1ddd45e05148b31af8dba9d943709935
Bubba <rob@stechstudio.com>
2019-02-22 17:58:13 -0500

    Artisan Package command

---
+++ src/Console/Package.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+/**
+ * User: bubba
+ * Date: 2019-01-31
+ * Time: 16:27
+ */
+
+namespace STS\Bref\Bridge\Console;
+
+use Illuminate\Console\Command;
+use STS\Bref\Bridge\Events\LambdaPackageRequested;
+
+class Package extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'bref:package';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Package (zip) the application in preparation for deployment, upload it to S3, and generate the .stack.yaml';
+
+    public function handle(): int
+    {
+        event((new LambdaPackageRequested)->setConsole($this));
+        return 0;
+    }
+}
