5cf7c3321af924e38b8af142e40027f7389dd6cf
Bubba <rob@stechstudio.com>
2019-02-27 17:00:12 -0500

    A few new helpers

--- src/helpers.php
+++ src/helpers.php
@@ -101,3 +101,60 @@ if (! function_exists('copyFolder')) {
         return true;
     }
 }
+
+if (! function_exists('exceptionToArray')) {
+    function exceptionToArray(?Throwable $e): array
+    {
+        if ($e === null) {
+            return [];
+        }
+        return [
+            'message' => $e->getMessage(),
+            'code' => $e->getCode(),
+            'file' => $e->getFile(),
+            'line' => $e->getLine(),
+            'stack_trace' => $e->getTrace(),
+            'previous' => exceptionToArray($e->getPrevious()),
+        ];
+    }
+}
+
+if (! function_exists('lambda')) {
+    function lambda(string $event, string $context): array
+    {
+        $app = require_once __DIR__ . '/bootstrap/app.php';
+
+        /*
+        |--------------------------------------------------------------------------
+        | Run The Artisan Application
+        |--------------------------------------------------------------------------
+        |
+        | When we run the console application, the current CLI command will be
+        | executed in this console and the response sent back to a terminal
+        | or another output device for the developers. Here goes nothing!
+        |
+        */
+
+        $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+
+        $results = $kernel->handle(
+            $input = new Symfony\Component\Console\Input\ArgvInput,
+            new Symfony\Component\Console\Output\ConsoleOutput
+        );
+
+        /*
+        |--------------------------------------------------------------------------
+        | Shutdown The Application
+        |--------------------------------------------------------------------------
+        |
+        | Once Artisan has finished running, we will fire off the shutdown events
+        | so that any final work may be done by the application before we shut
+        | down the process. This is the last thing to happen to the request.
+        |
+        */
+
+        $kernel->terminate($input, $status);
+
+        return $results;
+    }
+}
