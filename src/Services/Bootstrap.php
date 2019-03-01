4ff1a917840a5b61de1ca0bedc7268873890eccb
Bubba <rob@stechstudio.com>
2019-02-28 20:11:43 -0500

    Not that, log this

--- src/Services/Bootstrap.php
+++ src/Services/Bootstrap.php
@@ -9,7 +9,6 @@
 namespace STS\Bref\Bridge\Services;
 
 use Bref\Runtime\PhpFpm;
-use Illuminate\Support\Facades\Log;
 use STS\AwsEvents\Events\ApiGatewayProxyRequest;
 use STS\AwsEvents\Events\Event;
 use STS\Bref\Bridge\Models\LambdaResult;
@@ -262,13 +261,9 @@ class Bootstrap
         // Initialize a new cURL resource for reporting.
         $this->initInvocationResult();
 
-        // If we have a body, it needs to be encoded within the response.
-        if (array_key_exists('body', $response)) {
-            $response['body'] = json_encode($response['body']);
-        }
         // Now we can encode everything.
         $response_json = json_encode($response);
-        Log::info($response_json);
+        self::consoleLog($response_json);
         // Set up curl
         curl_setopt(
             $this->result,
