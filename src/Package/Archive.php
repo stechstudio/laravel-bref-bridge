9fbe1dd3d6e42b7c6ee0404f019cdb5fc8075003
Bubba <rob@stechstudio.com>
2019-10-15 12:01:27 -0400

    Update bootstrap path, take two

--- src/Package/Archive.php
+++ src/Package/Archive.php
@@ -127,7 +127,7 @@ class Archive
         $package        = self::make();
         $vendorFileList = $package->collectComposerLibraries();
         $package->addCollection($vendorFileList);
-        $package->addFile(__DIR__.'/../../vendor/bin/bootstrap',
+        $package->addFile(base_path('/vendor/bin/bootstrap'),
             'bootstrap');
         $package->setPermissions(
             'bootstrap',
